546d40b3c446c37d7796a73526bae58e36886584
Cong Yue <yuecong1104@gmail.com>
2016-04-30 15:54:37 -0700

    Update sort script

--- test_hadoop_sort.sh
+++ test_hadoop_sort.sh
@@ -1,12 +1,9 @@
-#!/bin/sh
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 -file ./mapper_project.py -mapper ./mapper_project.py \
 -file ./reducer_project.py -reducer ./reducer_project.py \
 -input /user/hadoop/sample1/input/* \
--output /user/hadoop/test-output1 \
--jobconf mapred.reduce.tasks=4 \
--numReduceTasks 4 \
+-output /user/hadoop/test-output2 \
 -jobconf stream.num.map.output.key.fields=2 \
--jonconf mapreduce.partition.keypartitioner.options=-K1,2 \
+-jobconf mapreduce.partition.keypartitioner.options=-K1,2 \
 -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner 
 

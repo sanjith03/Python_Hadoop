f884044820eb218cba9462924317600b39ca82d7
Cong Yue <yuecong@users.noreply.github.com>
2016-04-29 17:52:28 -0700

    Update test_hadoop_sort.sh

--- test_hadoop_sort.sh
+++ test_hadoop_sort.sh
@@ -6,7 +6,7 @@ hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 -output /user/hadoop/test-output1 \
 -jobconf mapred.reduce.tasks=4 \
 -numReduceTasks 4 \
--D stream.num.map.output.key.fields=2 \
--D mapreduce.partition.keypartitioner.options=-K1,2 \
+-jobconf stream.num.map.output.key.fields=2 \
+-jonconf mapreduce.partition.keypartitioner.options=-K1,2 \
 -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner 
 

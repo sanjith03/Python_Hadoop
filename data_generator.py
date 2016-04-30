7dab3806261e45992ccfe6b8eb7fe9499bfbd785
cloudera <cloudera@quickstart.cloudera>
2016-04-30 10:20:12 -0700

    Change to generage 1Gb file

--- data_generator.py
+++ data_generator.py
@@ -7,7 +7,7 @@ n=Factory.create()
 file=open("input_data.csv","a")
 w=csv.writer(file)
 w.writerow(('ID#','Name','College','Age'))
-for i in range(1000):
+for i in range(1000*350*100):
   
     w.writerow((i+1,n.name(),random.choice(['ITU','Stanford','SJSU','SCU']),random.choice(range(18,35))))
 file.close()

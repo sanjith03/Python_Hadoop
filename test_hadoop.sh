f555bb8048044027663d17005d316a8afe6e66b0
Cong Yue <yuecong@users.noreply.github.com>
2016-05-01 09:39:45 -0700

    Update test_hadoop.sh

--- test_hadoop.sh
+++ test_hadoop.sh
@@ -5,9 +5,7 @@ hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 -jobconf mapreduce.partition.keypartitioner.options=-K1,2 \
 -jobconf mapred.reduce.tasks=4 \ 
 -numReduceTasks 10 \
--file mapper_project.py \   
--mapper mapper_project.py \
--file reducer_project.py   \
--reducer reducer_project.py \
+-file mapper_project.py -mapper mapper_project.py \
+-file reducer_project.py  -reducer reducer_project.py \
 -input project/sample1/input/* \
 -output project/sample/output3
